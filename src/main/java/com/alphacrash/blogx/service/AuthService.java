753dd14ee85c79bc9193e3c86a2e949735ce0eb0
alphacrash <sudhansu.ace@gmail.com>
2021-12-06 15:08:15 +0530

    Added encoding of password

--- src/main/java/com/alphacrash/blogx/service/AuthService.java
+++ src/main/java/com/alphacrash/blogx/service/AuthService.java
@@ -4,6 +4,7 @@ import com.alphacrash.blogx.dto.RegisterRequest;
 import com.alphacrash.blogx.model.User;
 import com.alphacrash.blogx.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,18 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(RegisterRequest registerRequest) {
         User user = new User();
         user.setUserName(registerRequest.getUsername());
         user.setPassword(registerRequest.getPassword());
-        user.setEmail(registerRequest.getEmail());
+        user.setEmail(encodePassword(registerRequest.getEmail()));
         userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
