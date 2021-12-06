336ee204af80cb99fe3de0bd36582bf39372387a
alphacrash <sudhansu.ace@gmail.com>
2021-12-06 12:53:24 +0530

    Project Initialized

---
+++ src/main/java/com/alphacrash/blogx/BlogXApplication.java
@@ -0,0 +1,13 @@
+package com.alphacrash.blogx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphacrash/blogx/BlogXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphacrash.blogx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
