6f6c2cd30fad7db03bf95aa1a2fd2df4b39f3301
alphacrash <sudhansu.ace@gmail.com>
2021-12-06 14:04:38 +0530

    Added Models

---
+++ src/main/java/com/alphacrash/blogx/model/Post.java
@@ -0,0 +1,71 @@
+package com.alphacrash.blogx.model;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Lob
+    @Column(nullable = false)
+    private String content;
+    @Column
+    private Instant createdOn;
+    @Column
+    private Instant updatedOn;
+    @Column(nullable = false)
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Instant createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Instant getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Instant updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
